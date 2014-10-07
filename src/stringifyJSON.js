7717d591a88ae98d1b3482eccd6a4ff2fd2841e8
owen-d <ow.diehl@gmail.com>
2014-10-07 18:31:48 -0400

    started working, still fixing small bugs in stringify.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,39 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'object' && obj !== null) {
+    if (obj.length !== 0 && obj.length !== undefined) {
+    var objKeys = Object.keys(obj);
+    var result = [];
+    for (var i = 0; i < objKeys.length; i++) {
+      var keyString = '"' + objKeys[i] + '"';
+      var objKeyValue = obj[objKeys[i]];
+      if (typeof objKeyValue === 'string') {
+        keyString = keyString + ': "' + objKeyValue + '"';
+        result.push(keyString);
+      }
+      else {
+        keyString = keyString + stringifyJSON(objKeyValue);
+        result.push(keyString);
+      }
+    }
+    return '{' + result.join(', ') + '}';
+  }
+  else {
+    if (obj.length !== 0) {
+    return typeof obj === 'string' ? '"' + obj + '"':
+    '' + obj;
+  }
+    else {
+      return typeof obj === 'object' ? '{}' : '[]';
+    }
+  }
+  }
+  else {
+    return typeof obj === 'string' ? '"' + obj + '"':
+    '' + obj;
+  }
+
+  
+
 };
