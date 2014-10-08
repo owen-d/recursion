7eedf5d8327008c9368a58d5a5ed7faa0d95e7a3
owen-d <ow.diehl@gmail.com>
2014-10-07 21:49:14 -0400

    Section 3 Recursion requirements met.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,40 +3,66 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-  if (typeof obj === 'object' && obj !== null) {
-    if (obj.length !== 0 && obj.length !== undefined) {
-    var objKeys = Object.keys(obj);
-    var result = [];
-    for (var i = 0; i < objKeys.length; i++) {
-      var keyString = '"' + objKeys[i] + '"';
-      var objKeyValue = obj[objKeys[i]];
-      if (typeof objKeyValue === 'string') {
-        keyString = keyString + ': "' + objKeyValue + '"';
-        result.push(keyString);
+  if (obj === undefined || typeof obj === 'function') {
+    return;
+  }
+  else if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        return '[]';
       }
       else {
-        keyString = keyString + stringifyJSON(objKeyValue);
+        //non empty array stuff
+        var result = [];
+        for (var i = 0; i < obj.length; i++) {
+          var arrString;
+          if (typeof obj[i] === 'string') {
+            result.push('"' + obj[i] + '"');
+          }
+          else {
+            result.push(stringifyJSON(obj[i]));
+          }
+        }
+      return '[' + result.join(',') + ']'; 
+      }
+    }
+    else if (obj.length === 0) {
+      return '{}';
+    }
+    else {
+      //non empty object stuff
+      var objKeys = Object.keys(obj);
+    var result = [];
+    for (var i = 0; i < objKeys.length; i++) {
+      var keyString = '"' + objKeys[i] + '":';
+      var objKeyValue = obj[objKeys[i]];
+      if (typeof objKeyValue === 'string') {
+        keyString = keyString + '"' + objKeyValue + '"';
+        result.push(keyString);
+      }
+      else { 
+        if (objKeyValue === undefined || typeof objKeyValue === 'function') {
+          keyString = "";
+        }
+        else {
+          keyString = keyString + stringifyJSON(objKeyValue);
+        }
         result.push(keyString);
       }
     }
-    return '{' + result.join(', ') + '}';
-  }
-  else {
-    if (obj.length !== 0) {
-    return typeof obj === 'string' ? '"' + obj + '"':
-    '' + obj;
-  }
-    else {
-      return typeof obj === 'object' ? '{}' : '[]';
+    var trimmedResult = result.filter(function(x){
+      return x !== "";
+    });
+    return '{' + trimmedResult.join(',') + '}';
     }
   }
+  else if (typeof obj === 'string') {
+    return '"' + obj + '"';
   }
   else {
-    return typeof obj === 'string' ? '"' + obj + '"':
-    '' + obj;
+    return ""+obj;
   }
-
-  
-
 };
